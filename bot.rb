498f7e5e78356c44c7b6c8a390e5368fb986f6ae
pleax <aka.pleax@gmail.com>
2010-09-06 14:55:13 +0000

    Implemented oauth authorization scenarios

--- bot.rb
+++ bot.rb
@@ -1,2 +1,29 @@
 require 'boot.rb'
 
+require 'config.rb'
+
+oauth = Twitter::OAuth.new(CONFIG[:oauth_token], CONFIG[:oauth_secret])
+
+if CONFIG[:access_token].nil? or CONFIG[:access_secret].nil?
+  request_token  = oauth.request_token.token
+  request_secret = oauth.request_token.secret
+
+  puts "[:] authorize bot on twitter with this url: #{oauth.request_token.authorize_url}"
+
+  puts "[:] what was the pin twitter provided you with?"
+  print "> "
+  pin = gets.chomp
+
+  oauth.authorize_from_request(request_token, request_secret, pin)
+
+  puts "[:] add these values to config.rb:"
+  puts ":access_token => #{oauth.access_token.token.inspect},"
+  puts ":access_secret => #{oauth.access_token.secret.inspect},"
+else
+  oauth.authorize_from_access(CONFIG[:access_token], CONFIG[:access_secret])
+end
+
+twitter = Twitter::Base.new(oauth)
+
+#twitter.update("He's alive!!")
+
---
+++ config.sample.rb
@@ -0,0 +1,8 @@
+
+CONFIG = {
+  :oauth_token => 'abcdefg',
+  :oauth_secret => 'hijklmnopqrstuvwxyz',
+
+  # :access_token => "blah-blah",
+  # :access_secret => "blah-blah",
+}
